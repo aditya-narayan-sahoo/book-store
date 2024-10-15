8f409f685652661783cbc633a2e4ca9e7942e3f3
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-10-16 00:05:09 +0530

    Add the news section

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import Banner from "./Banner";
 import Recommended from "./Recommended";
 import TopSellers from "./TopSellers";
+import News from "./News";
 const Home = () => {
   return (
     <>
       <Banner />
       <TopSellers />
       <Recommended />
+      <News />
     </>
   );
 };
