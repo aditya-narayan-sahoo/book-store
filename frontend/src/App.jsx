245fd58c2f2daa5432a44aec80a7113da6e9fff8
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-10-16 00:08:19 +0530

    Add the Page's Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
         <Outlet />
       </main>
-      <nav>Footer</nav>
+      <Footer />
     </>
   );
 };
