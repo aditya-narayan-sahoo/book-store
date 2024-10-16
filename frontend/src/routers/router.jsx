ed1af8dc98c3b2f12fbec89a4278a4be0a0972bd
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-10-16 10:10:14 +0530

    Add login and register pages

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/home/Home";
+import Login from "../components/Login";
+import Register from "../components/Register";
 
 const router = createBrowserRouter([
   {
@@ -8,8 +10,8 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/orders", element: <div>Orders</div> },
-      { path: "/about", element: <div>About</div> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
     ],
   },
 ]);
